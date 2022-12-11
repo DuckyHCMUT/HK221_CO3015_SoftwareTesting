37d0f116ddb82638d67a0d059e7519dd9bfcfe1e
Tran Quoc Viet <tqviet157@gmail.com>
2022-12-12 03:22:31 +0700

    Added A01 and A02 testcases
    Voided by lecturer, so just for reference

---
+++ CO3015_Assignment3/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CO3015_Assignment3
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            A01AddMenu driverA01 = new A01AddMenu();
+            driverA01.RunTest();
+
+            A02OrderAction driverA02 = new A02OrderAction();
+            driverA02.RunTest();
+        }
+    }
+}
