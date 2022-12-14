2c7cbd9824cf8741cdbf17910276c05d0bb78580
Tran Quoc Viet <tqviet157@gmail.com>
2022-12-14 19:11:24 +0700

    Reformatted for case B06

--- CO3015_Assignment3/B06BlockUser.cs
+++ CO3015_Assignment3/B06BlockUser.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using OfficeOpenXml;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.Configuration;
 
@@ -8,12 +9,26 @@ namespace CO3015_Assignment3
     {
         private readonly string? _userName = ConfigurationManager.AppSettings["username"];
         private readonly string? _password = ConfigurationManager.AppSettings["password"];
+        private readonly string? _projectDirectory;
+
+        public B06BlockUser()
+        {
+            // Define License Context for EPPlus package
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            _projectDirectory = Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.FullName;
+
+            if (string.IsNullOrEmpty(_projectDirectory))
+            {
+                throw new DirectoryNotFoundException($"{_projectDirectory} not found!");
+            }
+        }
 
         public void RunTest()
         {
-            TcB0601();
-            TcB0602();
-            TcB0603();
+            //TcB0601();
+            //TcB0602();
+            //TcB0603();
             TcB0604();
         }
 
@@ -34,12 +49,74 @@ namespace CO3015_Assignment3
             driver.FindElement(By.Name("submit")).Click();
             Thread.Sleep(5000);
         }
+        private string? GetCellDataFromExcelFile(string excelFileName, string cell)
+        {
+            using var xlPackage = new ExcelPackage(new FileInfo(
+                Path.Combine(_projectDirectory ?? throw new InvalidOperationException(), $"resource\\{excelFileName}")));
+
+            var myWorksheet = xlPackage.Workbook.Worksheets.First();
+            return myWorksheet.Cells[cell].Select(x => x.Value.ToString()).First();
+        }
 
         private void RecoverBlockedUser(IWebDriver driver)
         {
+            var targetId = GetCellDataFromExcelFile("B06_data.xlsx", "B3");
+
+            if (string.IsNullOrEmpty(targetId))
+            {
+                throw new NullReferenceException("Null Target Id");
+            }
+
+            Login(driver);
+
+            Console.WriteLine("Restoration started");
+
+            driver.FindElement(By.XPath("//a[contains(@id, 'message-drawer-toggle')]")).Click();
+            Thread.Sleep(2000);
+
+            driver.FindElement(By.XPath("//button[contains(@aria-controls, 'view-overview-messages-target')]"))
+                .Click();
+            Thread.Sleep(2000);
+
+            driver.FindElement(By.XPath($"//a[contains(@data-conversation-id, '{targetId}')]")).Click();
+            Thread.Sleep(2000);
+
+            var requestUnblockBtnElement = driver.FindElement(By.XPath("//button[contains(@data-action, 'request-unblock')]"));
+
+            if (requestUnblockBtnElement.Displayed)
+            {
+                requestUnblockBtnElement.Click();
+                Thread.Sleep(2000);
+            }
+            else
+            {
+                Console.WriteLine("Not need to restore");
+                return;
+            }
+
+            driver.FindElement(By.XPath("//button[contains(@data-action, 'confirm-unblock')]")).Click();
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Restoration finished");
+        }
+
+        /// <summary>
+        /// Test case for Block admin
+        /// </summary>
+        public void TcB0601()
+        {
+            IWebDriver driver = new ChromeDriver();
+
             try
             {
-                Console.WriteLine($"Performing system restore before execution of {nameof(TcB0604)} started");
+                var targetId = GetCellDataFromExcelFile("B06_data.xlsx", "B2");
+
+                if (string.IsNullOrEmpty(targetId))
+                {
+                    throw new NullReferenceException("Null Target Id");
+                }
+
+                Console.WriteLine($"Test case {nameof(TcB0601)} started");
 
                 Login(driver);
 
@@ -50,40 +127,7 @@ namespace CO3015_Assignment3
                     .Click();
                 Thread.Sleep(2000);
 
-                driver.FindElement(By.XPath("//a[contains(@data-conversation-id, '233963')]")).Click();
-                Thread.Sleep(2000);
-
-                driver.FindElement(By.XPath("//button[contains(@data-action, 'request-unblock')]")).Click();
-                Thread.Sleep(2000);
-
-                driver.FindElement(By.XPath("//button[contains(@data-action, 'confirm-unblock')]")).Click();
-                Thread.Sleep(2000);
-
-                Console.WriteLine($"Restore completed, test case {nameof(TcB0604)} is starting soon...");
-            }
-            catch (ElementNotInteractableException)
-            {
-                throw new ElementNotInteractableException();
-            }
-        }
-
-        public void TcB0601()
-        {
-            IWebDriver driver = new ChromeDriver();
-            Login(driver);
-
-            try
-            {
-                Console.WriteLine($"Test case {nameof(TcB0601)} started");
-
-                driver.FindElement(By.XPath("//a[contains(@id, 'message-drawer-toggle')]")).Click();
-                Thread.Sleep(2000);
-
-                driver.FindElement(By.XPath("//button[contains(@aria-controls, 'view-overview-messages-target')]"))
-                    .Click();
-                Thread.Sleep(2000);
-
-                driver.FindElement(By.XPath("//a[contains(@data-conversation-id, '66963')]")).Click();
+                driver.FindElement(By.XPath($"//a[contains(@data-conversation-id, '{targetId}')]")).Click();
                 Thread.Sleep(2000);
 
                 driver.FindElement(By.Id("conversation-actions-menu-button")).Click();
@@ -94,8 +138,8 @@ namespace CO3015_Assignment3
 
                 driver.FindElement(By.XPath("//button[contains(@data-action, 'okay-confirm')]")).Click();
                 Thread.Sleep(2000);
-                
-                Console.WriteLine($"Test case {nameof(TcB0601)} ended");
+
+                Console.WriteLine($"Test case {nameof(TcB0601)} executed successfully");
             }
             catch (Exception e)
             {
@@ -105,141 +149,116 @@ namespace CO3015_Assignment3
             finally
             {
                 driver.Close();
+                Console.WriteLine("==========================================================");
             }
-            Console.WriteLine("==========================================================");
         }
 
+        /// <summary>
+        /// Test case for Cancel blocking
+        /// </summary>
         public void TcB0602()
         {
             IWebDriver driver = new ChromeDriver();
+            RecoverBlockedUser(driver);
 
             try
             {
-                RecoverBlockedUser(driver);
+                Console.WriteLine($"Test case {nameof(TcB0602)} started");
+
+                driver.FindElement(By.Id("conversation-actions-menu-button")).Click();
+                Thread.Sleep(2000);
+
+                driver.FindElement(By.LinkText("Block user")).Click();
+                Thread.Sleep(2000);
+
+                driver.FindElement(By.XPath("//button[contains(@data-action, 'cancel-confirm')]")).Click();
+                Thread.Sleep(2000);
+
+                Console.WriteLine($"Test case {nameof(TcB0602)} executed successfully");
             }
-            // If this exception is caught, the restoration step is not necessary
-            catch (ElementNotInteractableException)
+            catch (Exception e)
             {
-                Console.WriteLine("Restoration is not required");
+                Console.WriteLine($"{nameof(TcB0602)}: {e.Message}");
+                Console.WriteLine($"Test case {nameof(TcB0602)} failed");
             }
             finally
             {
-                try
-                {
-                    Console.WriteLine($"Test case {nameof(TcB0602)} started");
-
-                    driver.FindElement(By.Id("conversation-actions-menu-button")).Click();
-                    Thread.Sleep(2000);
-
-                    driver.FindElement(By.LinkText("Block user")).Click();
-                    Thread.Sleep(2000);
-
-                    driver.FindElement(By.XPath("//button[contains(@data-action, 'cancel-confirm')]")).Click();
-                    Thread.Sleep(2000);
-
-                    Console.WriteLine($"Test case {nameof(TcB0602)} ended");
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine($"{nameof(TcB0602)}: {e.Message}");
-                    Console.WriteLine($"Test case {nameof(TcB0602)} failed");
-                }
-                finally
-                {
-                    driver.Close();
-                }
+                driver.Close();
+                Console.WriteLine("==========================================================");
             }
-            Console.WriteLine("==========================================================");
         }
 
+        /// <summary>
+        /// Test case for Attempt to block a blocked user
+        /// </summary>
         public void TcB0603()
         {
             IWebDriver driver = new ChromeDriver();
+            RecoverBlockedUser(driver);
 
             try
             {
-                RecoverBlockedUser(driver);
+                Console.WriteLine($"Test case {nameof(TcB0603)} started");
+
+                driver.FindElement(By.Id("conversation-actions-menu-button")).Click();
+                Thread.Sleep(2000);
+
+                driver.FindElement(By.LinkText("Block user")).Click();
+                Thread.Sleep(2000);
+
+                driver.FindElement(By.XPath("//button[contains(@data-action, 'confirm-block')]")).Click();
+                Thread.Sleep(2000);
+
+                driver.FindElement(By.Id("conversation-actions-menu-button")).Click();
+                Thread.Sleep(2000);
+
+                Console.WriteLine($"Test case {nameof(TcB0603)} executed successfully");
             }
-            // If this exception is caught, the restoration step is not necessary
-            catch (ElementNotInteractableException)
+            catch (Exception e)
             {
-                Console.WriteLine("Restoration is not required");
+                Console.WriteLine($"{nameof(TcB0603)}: {e.Message}");
+                Console.WriteLine($"Test case {nameof(TcB0603)} failed");
             }
             finally
             {
-                try
-                {
-                    Console.WriteLine($"Test case {nameof(TcB0603)} started");
-
-                    driver.FindElement(By.Id("conversation-actions-menu-button")).Click();
-                    Thread.Sleep(2000);
-
-                    driver.FindElement(By.LinkText("Block user")).Click();
-                    Thread.Sleep(2000);
-
-                    driver.FindElement(By.XPath("//button[contains(@data-action, 'confirm-block')]")).Click();
-                    Thread.Sleep(2000);
-
-                    driver.FindElement(By.Id("conversation-actions-menu-button")).Click();
-                    Thread.Sleep(2000);
-
-                    Console.WriteLine($"Test case {nameof(TcB0603)} ended");
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine($"{nameof(TcB0603)}: {e.Message}");
-                    Console.WriteLine($"Test case {nameof(TcB0603)} failed");
-                }
-                finally
-                {
-                    driver.Close();
-                }
+                driver.Close();
+                Console.WriteLine("==========================================================");
             }
-            Console.WriteLine("==========================================================");
         }
 
-
+        /// <summary>
+        /// Test case for Block user successfully
+        /// </summary>
         public void TcB0604()
         {
             IWebDriver driver = new ChromeDriver();
+            RecoverBlockedUser(driver);
 
             try
             {
-                RecoverBlockedUser(driver);
+                Console.WriteLine($"Test case {nameof(TcB0604)} started");
+
+                driver.FindElement(By.Id("conversation-actions-menu-button")).Click();
+                Thread.Sleep(2000);
+
+                driver.FindElement(By.LinkText("Block user")).Click();
+                Thread.Sleep(2000);
+
+                driver.FindElement(By.XPath("//button[contains(@data-action, 'confirm-block')]")).Click();
+                Thread.Sleep(2000);
+
+                Console.WriteLine($"Test case {nameof(TcB0604)} executed successfully");
             }
-            // If this exception is caught, the restoration step is not necessary
-            catch (ElementNotInteractableException)
+            catch (Exception e)
             {
-                Console.WriteLine("Restoration is not required");
+                Console.WriteLine($"{nameof(TcB0604)}: {e.Message}");
+                Console.WriteLine($"Test case {nameof(TcB0604)} failed");
             }
             finally
             {
-                try
-                {
-                    Console.WriteLine($"Test case {nameof(TcB0604)} started");
-
-                    driver.FindElement(By.Id("conversation-actions-menu-button")).Click();
-                    Thread.Sleep(2000);
-
-                    driver.FindElement(By.LinkText("Block user")).Click();
-                    Thread.Sleep(2000);
-
-                    driver.FindElement(By.XPath("//button[contains(@data-action, 'confirm-block')]")).Click();
-                    Thread.Sleep(2000);
-
-                    Console.WriteLine($"Test case {nameof(TcB0604)} ended");
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine($"{nameof(TcB0604)}: {e.Message}");
-                    Console.WriteLine($"Test case {nameof(TcB0604)} failed");
-                }
-                finally
-                {
-                    driver.Close();
-                }
+                driver.Close();
+                Console.WriteLine("==========================================================");
             }
-            Console.WriteLine("==========================================================");
         }
     }
 }
